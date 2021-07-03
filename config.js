aebc50c9597eb69a9beea2cf6e0c106955092f85
thienluan1997 <86874813+thienluan1997@users.noreply.github.com>
2021-07-03 17:01:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,16 +4,16 @@ const CONFIG = {
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'Bae chọn đi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều hơn hôm nay và it hơn ngày mai 😘😘',
-    messDesc: 'Anh ở  xa cũng đang nhớ em nhiều .',
+    reply: 'Vì anh đẹp trai hehe <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều hơn hôm nay và ít hơn ngày mai 😘😘',
+    messDesc: 'Anh ở xa cũng đang nhớ em nhiều .',
     btnAccept: 'Nhớ Uyên <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
